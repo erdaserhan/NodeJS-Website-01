ba67d662bb01aaa9651cdfa395d81bd62602746d
erdaserhan <erhan209@gmail.com>
2024-09-05 15:04:59 +0200

    express updated

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function(request, response, next){
+    response.render('accueil.ejs');
+});
+
+app.use(function(request, response, next) {
+    response.status(404).render('404.ejs', {msg: "La page demandée n'existe pas"});
+});
